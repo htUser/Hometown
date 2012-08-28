b400689f2af4e1d89c88b1673eb8683592c2f3b1
Sarah Mettler <mettlsa@gmail.com>
2012-08-27 21:26:13 -0400

    updated to remove all savepoint code and add a db check for the listing before you add to the db, only add if not already in

--- src/com/platform/hometown/Client/SelectListingsController.java
+++ src/com/platform/hometown/Client/SelectListingsController.java
@@ -6,6 +6,7 @@ import com.platform.api.Controller;
 import com.platform.api.ControllerResponse;
 import com.platform.api.Functions;
 import com.platform.api.Parameters;
+import com.platform.api.ParametersIterator;
 import com.platform.api.Result;
 import com.platform.hometown.proximity.processProximity;
 
@@ -14,6 +15,7 @@ public class SelectListingsController implements Controller {
 	@Override
 	public ControllerResponse execute(HashMap params) throws Exception {
 		
+		Functions.debug("Inside SelectListingController");
 		String myAction = (String)params.get("action");
 		String cId; //campaign id
 		
@@ -36,54 +38,53 @@ public class SelectListingsController implements Controller {
 			resp.setTargetPage("listings.jsp");
 			
 		} else if(myAction.equals("save")==true){
-			
+			Functions.debug("Saving Listings");
 			//for each keyword set selected if it is selected on the page
 			for(int i=0; i<campaign.getKeyWords().size();i++){
 				
 				for(int j=0; j<campaign.getKeyWords().get(i).getCountiesAvailable().size();j++){
 					
-					//Bo defaults to selected true, so reset to false first, then check page and set
-					campaign.getKeyWords().get(i).getCountiesAvailable().get(j).setSelected(false);
 					
 					if(((String)params.get(i+"county"+j)!=null) &&((String)params.get(i+"county"+j)).length()!=0){
 						
-						campaign.getKeyWords().get(i).getCountiesAvailable().get(j).setSelected(true);	
-						
-						//String savePtName = "savePoint"+i;
-						
-						//Functions.addSavePoint(savePtName);
-						//Functions.debug("Save point exists "+Functions.doesSavePointExist("savePoint"+i));
-						
-						//save to db
-						Parameters newListingParams = Functions.getParametersInstance();
-						newListingParams.add("service_t", campaign.getServiceType());
-						newListingParams.add("location_county", campaign.getKeyWords().get(i).getCountiesAvailable().get(j).getClientCountyId());	
-						newListingParams.add("campaign_keyword", campaign.getKeyWords().get(i).getCampaignKeyWordsJunctionId());				
-						newListingParams.add("tracking_number", campaign.getCampaignId());
-						newListingParams.add("county_lookup", campaign.getKeyWords().get(i).getCountiesAvailable().get(j).getCountyId());
-						newListingParams.add("campaign_name", campaign.getCampaignName());
-						newListingParams.add("client2", campaign.getClientLocation().getClient().getId());
-						
-						Result result = Functions.addRecord("CSA_Tracking", newListingParams);
-						
-						int resultCode = result.getCode();
-						
-						if(resultCode < 0)
-						{
-						    // Some error happened.
-						    String msg = "Listing could not be added";
-						    Functions.debug(msg + ":\n" + result.getMessage());  	// Log details & ignore error
-						    resp.setTargetPage("success.jsp");
-						    
-						    //Functions.debug("Save point name "+ savePtName +" exists "+Functions.doesSavePointExist(savePtName));
+						//first check if listing is already in db
+						Result checkResult = Functions.searchRecords("CSA_Tracking", "id","client2 equals '" + campaign.getClientLocation().getClient().getId() + "' AND county_lookup equals '" + campaign.getKeyWords().get(i).getCountiesAvailable().get(j).getCountyId() + "' AND tracking_number equals '" + campaign.getCampaignId() + "' AND campaign_keyword equals '" + campaign.getKeyWords().get(i).getCampaignKeyWordsJunctionId() + "' AND location_county equals '" +campaign.getKeyWords().get(i).getCountiesAvailable().get(j).getClientCountyId() + "'");
+						if(checkResult.getCode()<0){
+							throw new Exception("Error searching for current listing. ");
+
+						}else if(checkResult.getCode()==0){
+							Functions.debug("Adding listing to db");
 							
-						    //if(Functions.doesSavePointExist(savePtName)){				    
-							//	Functions.rollbackToSavePoint(savePtName);
-							//}
-						}
-						else
-						{
-							resp.setTargetPage("success.jsp");
+							//only if no records found save to db
+							Parameters newListingParams = Functions.getParametersInstance();
+							newListingParams.add("service_t", campaign.getServiceType());
+							newListingParams.add("location_county", campaign.getKeyWords().get(i).getCountiesAvailable().get(j).getClientCountyId());	
+							newListingParams.add("campaign_keyword", campaign.getKeyWords().get(i).getCampaignKeyWordsJunctionId());				
+							newListingParams.add("tracking_number", campaign.getCampaignId());
+							newListingParams.add("county_lookup", campaign.getKeyWords().get(i).getCountiesAvailable().get(j).getCountyId());
+							newListingParams.add("campaign_name", campaign.getCampaignName());
+							newListingParams.add("client2", campaign.getClientLocation().getClient().getId());
+							
+							Result result = Functions.addRecord("CSA_Tracking", newListingParams);
+							
+							int resultCode = result.getCode();
+							
+							if(resultCode < 0)
+							{
+							    // Some error happened.
+							    String msg = "Listing could not be added";
+							    Functions.debug(msg + ":\n" + result.getMessage());  	// Log details 
+							    Functions.throwError(msg);
+							    resp.setTargetPage("error.jsp");
+							    
+							   
+							}
+							else
+							{
+								resp.setTargetPage("success.jsp");
+							}
+						} else {
+							resp.setTargetPage("success.jsp");	
 						}
 						
 					}
