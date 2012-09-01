8ff3697dfb5c20337748d5f7dc0138f942ed6146
Sarah Mettler <mettlsa@gmail.com>
2012-08-31 22:31:17 -0400

    added update functionality

--- src/com/platform/hometown/Client/SelectBuildByKeyWordController.java
+++ src/com/platform/hometown/Client/SelectBuildByKeyWordController.java
@@ -17,7 +17,7 @@ public class SelectBuildByKeyWordController implements Controller {
 	
 	@Override
 	public ControllerResponse execute(HashMap params) throws Exception {
-		Logger.info("in theSelectBuildByKeyWordController" , "debug");
+		Logger.info("in theSelectBuildByKeyWordController" , debug_category);
 		
 		String cId; //campaign id
 		String keyWordId;
@@ -59,40 +59,46 @@ public class SelectBuildByKeyWordController implements Controller {
 				
 				for(int j=0; j<campaign.getCurrentListings().get(i).getPagesAvailable().size();j++){
 					
-					//Bo defaults to selected true, so reset to false first, then check page and set
-					campaign.getCurrentListings().get(i).getPagesAvailable().get(j).setSelected(false);
-					
 					if(((String)params.get(i+"page"+j)!=null) &&((String)params.get(i+"page"+j)).length()!=0){
-						//Functions.debug("Inside the save if");
-						campaign.getCurrentListings().get(i).getPagesAvailable().get(j).setSelected(true);	
-												
-						//save to db
-						Parameters newBuildParams = Functions.getParametersInstance();
-						newBuildParams.add("page", campaign.getCurrentListings().get(i).getPagesAvailable().get(j).getPageId());
-						newBuildParams.add("campaign", campaign.getCampaignId());	
-						newBuildParams.add("csa_tracking", campaign.getCurrentListings().get(i).getListingId());	
-						newBuildParams.add("distance", campaign.getCurrentListings().get(i).getPagesAvailable().get(j).getDistance());
-						newBuildParams.add("prox_rating", campaign.getCurrentListings().get(i).getPagesAvailable().get(j).getProximity());
+							
+						//first check if build is already in db
+						Result checkResult = Functions.searchRecords("Build", "id","page equals '" + campaign.getCurrentListings().get(i).getPagesAvailable().get(j).getPageId() + "' AND campaign equals '" + campaign.getCampaignId() + "' AND csa_tracking equals '" + campaign.getCurrentListings().get(i).getListingId() + "'");
+						if(checkResult.getCode()<0){
+							String msg = "Error searching for current build.";
+							Logger.info(msg + ":\n" + checkResult.getMessage(), debug_category); 
+							Functions.throwError(msg);
+							resp.setTargetPage("error.jsp");
+	
+						}else if(checkResult.getCode()==0){
+							Functions.debug("adding the build");
+							//only if no records found save to db
+							Parameters newBuildParams = Functions.getParametersInstance();
+							newBuildParams.add("page", campaign.getCurrentListings().get(i).getPagesAvailable().get(j).getPageId());
+							newBuildParams.add("campaign", campaign.getCampaignId());	
+							newBuildParams.add("csa_tracking", campaign.getCurrentListings().get(i).getListingId());	
+							newBuildParams.add("distance", campaign.getCurrentListings().get(i).getPagesAvailable().get(j).getDistance());
+							newBuildParams.add("prox_rating", campaign.getCurrentListings().get(i).getPagesAvailable().get(j).getProximity());
+							
 						
-						
-						
-						Result result = Functions.addRecord("Build", newBuildParams);
-						int resultCode = result.getCode();
-						if(resultCode < 0)
-						{
-						    // Some error happened.
-						    String msg = "Build could not be added";
-						    Functions.debug(msg + ":\n" + result.getMessage());  // Log details
-						    resp.setTargetPage("success.jsp");
-						    //Functions.throwError(msg + ".");                     // Ignore error
-						}
-						else
-						{	
-							String msg = "New build id is";
-						    Functions.debug(msg + result.getId());  
-						    
-							resp.setTargetPage("success.jsp");
-						}
+							Result result = Functions.addRecord("Build", newBuildParams);
+							int resultCode = result.getCode();
+							if(resultCode < 0)
+							{
+								// Some error happened.
+							    String msg = "Build could not be added";
+							    Functions.debug(msg + ":\n" + result.getMessage());  // Log details
+							    resp.setTargetPage("error.jsp");
+							    Functions.throwError(msg + ".");  
+							}
+							else
+							{	
+								String msg = "New build id is";
+							    Functions.debug(msg + result.getId());  
+							    resp.setTargetPage("success.jsp");
+							}
+						} else {
+							resp.setTargetPage("success.jsp");	
+						}	
 						
 					}
 				
